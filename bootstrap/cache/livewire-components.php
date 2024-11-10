f951cd4f3c2eea5d613f903aa19b881d9778daab
Prabhu Pathak <113412091+prabhu-ji@users.noreply.github.com>
2024-11-10 20:52:14 +0530

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'admin' => 'App\\Http\\Livewire\\Admin',
+  'createseat' => 'App\\Http\\Livewire\\Createseat',
+  'editseat' => 'App\\Http\\Livewire\\Editseat',
+  'logout' => 'App\\Http\\Livewire\\Logout',
+  'profile' => 'App\\Http\\Livewire\\Profile',
+  'seatlayout' => 'App\\Http\\Livewire\\Seatlayout',
+  'signup' => 'App\\Http\\Livewire\\Signup',
+  'userlogin' => 'App\\Http\\Livewire\\Userlogin',
+  'userlogout' => 'App\\Http\\Livewire\\Userlogout',
+);
